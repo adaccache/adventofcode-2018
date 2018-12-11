342735c8ec110797868b0c747b7e2e2ec716609d
adaccache <adaccache@payments.ca>
2018-12-10 22:21:11 -0500

    completed part 1

--- 01/src/Main.java
+++ 01/src/Main.java
@@ -1,16 +1,18 @@
 import data.FileReader;
 
 import java.util.List;
+import java.util.Optional;
 
 class Main
 {
     public static void main(String args[])
     {
         FileReader fileReader = new FileReader();
-        List<String> data =  fileReader.getData();
+        List<Integer> data =  fileReader.getData();
 
+        Optional<Integer> total = data.stream().reduce((a, b) -> a + b);
 
-        System.out.println(data);
+        System.out.println(total);
 
     }
 } 
--- 01/src/data/FileReader.java
+++ 01/src/data/FileReader.java
@@ -5,17 +5,16 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public class FileReader {
 
-    public List<String> getData(){
+    public List<Integer> getData(){
         String fileName = "/home/tony/Projects/adventofcode-2018/01/src/data/input.txt";
-        List<String> respList = new ArrayList<>();
+        List<Integer> respList = new ArrayList<>();
         //read file into stream, try-with-resources
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-            stream.forEach(respList::add);
+            stream.forEach(a ->respList.add(Integer.parseInt(a)));
         } catch (IOException e) {
             e.printStackTrace();
         }
